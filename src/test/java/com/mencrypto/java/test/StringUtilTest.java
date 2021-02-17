222bc802b3890e75a828ca76bb24698981e62324
mencrypto <richard.zone@hotmail.com>
2021-02-17 17:53:33 -0600

    Se crea el proyecto de test de pruebas con Java en el IDE Intellij, en donde se define una clase que simplemente imprime el número de veces que se le indique una palabra, y con la clase StringUtilTest se le hace una prueba, mediante el método assertEquals que arroja errores con el detalle del problema, para entrar en la rutina de fallo, editar el for de la clase StringUtil agregando por ejemplo un ciclo más en el for

---
+++ src/test/java/com/mencrypto/java/test/StringUtilTest.java
@@ -0,0 +1,17 @@
+package com.mencrypto.java.test;
+
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result= StringUtil.repeat("hola",  3);
+        assertEquals(result,"holaholahola");
+
+        String result2 = StringUtil.repeat("hola",1);
+        assertEquals(result2,"hola");
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected))
+            throw new RuntimeException((actual + " is no equal to " + expected));
+    }
+}
